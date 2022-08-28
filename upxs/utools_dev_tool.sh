99f1be27b6b06c8b1224d180ad0bb2b336c63ea3
ChandlerVer5 <benrenxw522@sina.com>
2022-08-28 18:33:06 +0800

    feat: 🎸 add shell file for utools dev tools

---
+++ upxs/utools_dev_tool.sh
@@ -0,0 +1,3 @@
+cd /Users/bing/Projects/utools-plugins/upxs
+asar p utools-dev-tool 3595594d07eaf4298c1c511d0b4cdfa9.asar
+mv -f 3595594d07eaf4298c1c511d0b4cdfa9.asar /Users/bing/Library/Application\ Support/uTools/plugins
